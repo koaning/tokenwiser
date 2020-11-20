05a4cb342bd1860421f26f8572fc018dd2c3fef2
koaning <vincentwarmerdam@gmail.com>
2020-11-20 19:37:31 +0100

    t

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-base_packages = ["gensim", "jellyfish", "pyphen", "scikit-learn"]
+base_packages = ["gensim>=3.8.3", "jellyfish>=0.8.2", "Pyphen>=0.10.0", "scikit-learn>=0.23.2"]
 
-dev_packages = ["flake8>=3.6.0", "pytest==4.0.2", "jupyter==1.0.0", "jupyterlab==0.35.4"]
+dev_packages = ["flake8>=3.6.0", "pytest>=4.0.2", "jupyter>=1.0.0", "jupyterlab>=0.35.4"]
 
 
 setup(
--- tests/test_prep/test_phonetic.py
+++ tests/test_prep/test_phonetic.py
@@ -4,5 +4,15 @@ from tokenwiser.prep import PhoneticPrep
 
 
 @pytest.mark.parametrize("x_in,x_out", [("haleluja", "H442"), ("hello there world", "H400 T600 W643")])
-def test_basic(x_in, x_out):
-    assert PhoneticPrep().encode_single(x_in) == x_out
+def test_soundex(x_in, x_out):
+    assert PhoneticPrep(kind="soundex").encode_single(x_in) == x_out
+
+
+@pytest.mark.parametrize("x_in,x_out", [("haleluja", "HLLJ"), ("hello there world", "HL 0R WRLT")])
+def test_metaphone(x_in, x_out):
+    assert PhoneticPrep(kind="metaphone").encode_single(x_in) == x_out
+
+
+@pytest.mark.parametrize("x_in,x_out", [("haleluja", "HALALAJ"), ("hello there world", "HAL TAR WARLD")])
+def test_nysiis(x_in, x_out):
+    assert PhoneticPrep(kind="nysiis").encode_single(x_in) == x_out
