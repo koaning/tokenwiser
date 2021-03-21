cb08011bcf0626b295d81f89d1e25659e8dcc610
koaning <vincentwarmerdam@gmail.com>
2021-03-21 15:51:28 +0100

    one-mmor-test

--- tests/test_wabbit.py
+++ tests/test_wabbit.py
@@ -1,5 +1,7 @@
 import numpy as np
 from tokenwiser.wabbit import VowpalWabbitClassifier
+from tokenwiser.pipeline import make_partial_pipeline
+from tokenwiser.textprep import Cleaner
 
 X = [
     "i really like this post",
@@ -13,6 +15,13 @@ X = [
 y = np.array([1, 1, 1, 0, 0, 0])
 
 
-def test_wabbit_shape_sensible():
+def test_wabbit_fit_shape_sensible():
     assert VowpalWabbitClassifier().fit(X, y).predict(X).shape[0] == 6
     assert VowpalWabbitClassifier().fit(X, y).predict_proba(X).shape == (6, 2)
+
+
+def test_wabbit_pipeline():
+    pipe = make_partial_pipeline(Cleaner(),
+                                 VowpalWabbitClassifier(n_loop=1, n_gram=1, learning_rate=0.1))
+    for i in range(5):
+        pipe.partial_fit(X, y, classes=list(set(y)))
