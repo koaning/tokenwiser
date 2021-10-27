dd04977aca370ab027b5b70d67b1f2347fef0d48
Vincent <vincentwarmerdam@gmail.com>
2021-10-27 19:33:02 +0200

    added-download-method

--- tokenwiser/textprep/_sentpiece.py
+++ tokenwiser/textprep/_sentpiece.py
@@ -1,5 +1,8 @@
+from urllib.error import HTTPError
+import urllib.request
 from typing import Union
 from pathlib import Path
+
 import sentencepiece as spm
 from sklearn.base import BaseEstimator
 
@@ -45,3 +48,24 @@ class SentencePiecePrep(TextPrep, BaseEstimator):
 
     def encode_single(self, x):
         return " ".join(self.spm.encode_as_pieces(x))
+    
+    @classmethod
+    def download(self, lang: str, vocab_size: int, filename: str = None):
+        """
+        Download a pre-trained model from the bpemb project.
+
+        You can see some examples of pre-trained models on the [English](https://nlp.h-its.org/bpemb/en/) sub-site.
+        There are many languages available, but you should take care that you pick the right 
+        vocabulary size. 
+
+        Arguments:
+            lang: language code
+            vocab_size: vocab size, can be 1000, 3000, 5000, 10000, 25000, 50000, 100000, 200000
+        """
+        url = f"https://bpemb.h-its.org/{lang}/{lang}.wiki.bpe.vs{vocab_size}.model"
+        if not filename:
+            filename = f"{lang}.wiki.bpe.vs{vocab_size}.model"
+        try:
+            urllib.request.urlretrieve(url=url, filename=filename)
+        except HTTPError:
+            raise ValueError(f"Double check if the language ({lang}) and voacb size ({vocab_size}) combo exist.")
